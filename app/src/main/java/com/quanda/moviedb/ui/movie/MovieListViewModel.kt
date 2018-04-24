16c37462d2cfb00638350160b78d255abe3dc02b
Anh Quan <dang.anh.quan@framgia.com>
2018-04-24 13:06:53 +0700

    demo-rxjava2

--- app/src/main/java/com/quanda/moviedb/ui/movie/MovieListViewModel.kt
+++ app/src/main/java/com/quanda/moviedb/ui/movie/MovieListViewModel.kt
@@ -3,13 +3,18 @@ package com.quanda.moviedb.ui.movie
 import android.app.Application
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import android.util.Log
 import com.quanda.moviedb.MovieDBApplication
 import com.quanda.moviedb.base.viewmodel.BaseDataLoadMoreRefreshViewModel
 import com.quanda.moviedb.constants.ApiParam
 import com.quanda.moviedb.data.model.Movie
 import com.quanda.moviedb.data.source.UserRepository
 import com.quanda.moviedb.data.source.remote.response.GetMovieListResponse
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class MovieListViewModel(application: Application,
@@ -48,5 +53,85 @@ class MovieListViewModel(application: Application,
                 onLoadFail(e)
             }
         });
+    }
+
+    fun demoRxJava() {
+        Observable.just(1, 2, 3, 4, 5, 6)
+                .doOnNext { Log.e("RXJAVA", "Emitting $it") }
+                .map { it -> it * 3 }
+                .filter { it -> it % 2 == 0 }
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { Log.e("RXJAVA", "Consuming $it") }
+
+        Observable.create<String> {
+
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+
+                }
+
+        Observable.concat(Observable.create<String> {
+            it.onNext("mon")
+            Thread.sleep(200)
+            it.onNext("tue")
+            Thread.sleep(400)
+            it.onNext("wed")
+        }, Observable.just(2, 3, 4).doOnNext {
+            Thread.sleep(100)
+        })
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.e("==> ", it.toString())
+                }
+
+
+        val observer1 = Observable.fromArray(1, 2, 3, 4, 5).doOnNext { Thread.sleep(500) }
+        val observer2 = Observable.fromArray(6, 7, 8, 9, 10).doOnNext { Thread.sleep(400) }
+        Observable.merge<Int>(observer1, observer2)
+                .subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.e("==> ", it.toString())
+                }
+
+        Observable.interval(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.e("==> ", it.toString())
+                }
+
+        Observable.create<Long> {
+            it.onNext(2)
+            it.onNext(5)
+        }.doAfterNext { Thread.sleep(3000) }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).doOnNext {
+                    Log.e("==> ", it.toString())
+                }
+                .observeOn(Schedulers.computation()).map { it ->
+                    it * it
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    Log.e("==> ", it.toString())
+                }
+
+        Observable.defer {
+            Observable.create<Long> {
+                it.onNext(2)
+                it.onNext(5)
+            }
+        }.doAfterNext { Thread.sleep(3000) }
+                .flatMap {
+                    Observable.just(it)
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .map { it -> it * it }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { Log.e("==> ", it.toString()) }
     }
 }
