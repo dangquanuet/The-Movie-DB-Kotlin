4963409773d40b5541378636f54672d45671c069
Anh Quan <dang.anh.quan@framgia.com>
2018-09-06 14:44:59 +0700

    update-dialog-utils

--- app/src/main/java/com/quanda/moviedb/utils/DialogUtils.kt
+++ app/src/main/java/com/quanda/moviedb/utils/DialogUtils.kt
@@ -1,7 +1,6 @@
 package com.quanda.moviedb.utils
 
 import android.content.Context
-import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.support.v7.app.AlertDialog
@@ -9,26 +8,20 @@ import com.quanda.moviedb.R
 
 object DialogUtils {
 
-//    fun createLoadingDialog(context: Context?, cancelable: Boolean): Dialog {
-//        val dialog = Dialog(context)
-//        dialog.setCancelable(cancelable)
-//        dialog.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
-//        dialog.setContentView(R.layout.layout_loading_dialog)
-//        return dialog
-//    }
-
     fun createLoadingDialog(context: Context?, cancelable: Boolean = false,
-            canceledOnTouchOutside: Boolean = false): AlertDialog? {
+                            canceledOnTouchOutside: Boolean = false): AlertDialog? {
         if (context == null) return null
-        return AlertDialog.Builder(context).setView(R.layout.layout_loading_dialog).create().apply {
-            setCancelable(cancelable)
-            setCanceledOnTouchOutside(canceledOnTouchOutside)
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        }
+        return AlertDialog.Builder(context)
+                .setView(R.layout.layout_loading_dialog)
+                .create().apply {
+                    setCancelable(cancelable)
+                    setCanceledOnTouchOutside(canceledOnTouchOutside)
+                    window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
     }
 
     fun showLoadingDialog(context: Context?, cancelable: Boolean = false,
-            canceledOnTouchOutside: Boolean = false): AlertDialog? {
+                          canceledOnTouchOutside: Boolean = false): AlertDialog? {
         if (context == null) return null
         val dialog: AlertDialog? = createLoadingDialog(context, cancelable)
         dialog?.show()
@@ -36,47 +29,51 @@ object DialogUtils {
     }
 
     fun showMessage(context: Context?, title: String? = null, message: String? = null,
-            textPositive: String? = null, textNegative: String? = null,
-            callback: DialogCallback? = null, cancelable: Boolean = false): AlertDialog? {
+                    textPositive: String? = null, positiveListener: (() -> Unit)? = null,
+                    textNegative: String? = null, negativeListener: (() -> Unit)? = null,
+                    cancelable: Boolean = false, canceledOnTouchOutside: Boolean = false
+    ): AlertDialog? {
         if (context == null) return null
-        val dialog: AlertDialog =
-                AlertDialog.Builder(context).apply {
-                    setTitle(title)
-                    setMessage(message)
-                    setPositiveButton(textPositive, callback?.onPositiveListener())
-                    setNegativeButton(textNegative, callback?.onNegativeListener())
-                    setCancelable(cancelable)
-                }.create()
-        dialog.show()
-        return dialog
+        return AlertDialog.Builder(context).apply {
+            setTitle(title)
+            setMessage(message)
+            setPositiveButton(textPositive) { dialog, which ->
+                positiveListener?.invoke()
+            }
+            setNegativeButton(textNegative) { dialog, which ->
+                negativeListener?.invoke()
+            }
+            setCancelable(cancelable)
+
+        }.create().apply {
+            setCanceledOnTouchOutside(canceledOnTouchOutside)
+            show()
+        }
     }
 
     fun showMessage(context: Context?, title: Int? = null, message: Int? = null,
-            textPositive: Int? = null, textNegative: Int? = null, callback: DialogCallback? = null,
-            cancelable: Boolean = false): AlertDialog? {
+                    textPositive: Int? = null, positiveListener: (() -> Unit)? = null,
+                    textNegative: Int? = null, negativeListener: (() -> Unit)? = null,
+                    cancelable: Boolean = false, canceledOnTouchOutside: Boolean = false
+    ): AlertDialog? {
         if (context == null) return null
-        val dialog: AlertDialog = AlertDialog.Builder(context).apply {
+        return AlertDialog.Builder(context).apply {
             if (title != null) setTitle(title)
             if (message != null) setMessage(message)
             if (textPositive != null) {
-                setPositiveButton(textPositive, callback?.onPositiveListener())
+                setPositiveButton(textPositive) { dialog, which ->
+                    positiveListener?.invoke()
+                }
             }
             if (textNegative != null) {
-                setNegativeButton(textNegative, callback?.onNegativeListener())
+                setNegativeButton(textNegative) { dialog, which ->
+                    negativeListener?.invoke()
+                }
             }
             setCancelable(cancelable)
-        }.create()
-        dialog.show()
-        return dialog
-    }
-}
-
-abstract class DialogCallback {
-    open fun onNegativeListener(): DialogInterface.OnClickListener? {
-        return null
-    }
-
-    open fun onPositiveListener(): DialogInterface.OnClickListener? {
-        return null
+        }.create().apply {
+            setCanceledOnTouchOutside(canceledOnTouchOutside)
+            show()
+        }
     }
 }
