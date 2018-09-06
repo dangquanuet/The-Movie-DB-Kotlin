e7cdec813e7600e8411077fbc915001541008338
Anh Quan <dang.anh.quan@framgia.com>
2018-09-06 16:02:57 +0700

    add-error-handler

--- app/src/main/java/com/quanda/moviedb/MainApplication.kt
+++ app/src/main/java/com/quanda/moviedb/MainApplication.kt
@@ -2,11 +2,9 @@ package com.quanda.moviedb
 
 import android.app.Activity
 import android.app.Application
-import com.crashlytics.android.Crashlytics
 import com.quanda.moviedb.di.DaggerAppComponent
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
-import io.fabric.sdk.android.Fabric
 import javax.inject.Inject
 
 class MainApplication : Application(), HasActivityInjector {
@@ -15,10 +13,6 @@ class MainApplication : Application(), HasActivityInjector {
 
     override fun onCreate() {
         super.onCreate()
-
-        if (BuildConfig.FLAVOR.equals("stg") || BuildConfig.FLAVOR.equals("prd")) {
-            Fabric.with(this, Crashlytics())
-        }
 
         DaggerAppComponent.builder()
                 .application(this)
