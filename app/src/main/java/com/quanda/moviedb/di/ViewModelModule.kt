5f7fbd3a333a8f5dbe85b45793046e9f6b049996
Anh Quan <dang.anh.quan@framgia.com>
2018-10-26 16:11:05 +0700

    update-viewmodel-module

--- app/src/main/java/com/quanda/moviedb/di/ViewModelModule.kt
+++ app/src/main/java/com/quanda/moviedb/di/ViewModelModule.kt
@@ -7,15 +7,15 @@ import com.quanda.moviedb.ui.screen.main.MainViewModel
 import com.quanda.moviedb.ui.screen.moviedetail.MovieDetailViewModel
 import com.quanda.moviedb.ui.screen.popularmovie.PopularMovieViewModel
 import com.quanda.moviedb.ui.screen.tv.TvListViewModel
-import org.koin.androidx.viewmodel.ext.koin.viewModel
+import org.koin.androidx.viewmodel.experimental.builder.viewModel
 import org.koin.dsl.module.module
 
 val viewModelModule = module(override = true) {
-    viewModel { MainActivityViewModel() }
-    viewModel { MainViewModel() }
-    viewModel { FavoriteMovieViewModel(get()) }
-    viewModel { LoginViewModel() }
-    viewModel { MovieDetailViewModel(get(), get()) }
-    viewModel { PopularMovieViewModel(get(), get()) }
-    viewModel { TvListViewModel(get()) }
+    viewModel<MainActivityViewModel>()
+    viewModel<MainViewModel>()
+    viewModel<FavoriteMovieViewModel>()
+    viewModel<LoginViewModel>()
+    viewModel<MovieDetailViewModel>()
+    viewModel<PopularMovieViewModel>()
+    viewModel<TvListViewModel>()
 }
