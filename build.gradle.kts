8ddb02b73c0bb71a19b1d7399b57dfe4eb01cc28
Anh Quan <dang.anh.quan@framgia.com>
2018-11-12 13:51:24 +0700

    update-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.0-alpha11")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.2.71")
+        classpath("com.android.tools.build:gradle:3.3.0-beta03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
