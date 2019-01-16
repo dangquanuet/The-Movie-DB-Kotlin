ee7e81ad3f996e44ce5e1358a9808b2223de31b4
Anh Quan <dangquanuet@gmail.com>
2019-01-16 22:37:55 +0700

    support-64-bit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
         minSdkVersion(19)
         compileSdkVersion(28)
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        ndk?.setAbiFilters( listOf("armeabi-v7a", "arm64-v8a" ,"x86", "x86_64"))
     }
 
     signingConfigs {
