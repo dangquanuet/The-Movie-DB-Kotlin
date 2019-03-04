fe35d357d32592e8171e8956f3808115d53e6b76
Anh Quan <dang.anh.quan@framgia.com>
2019-03-04 11:15:53 +0700

    add-launch-activity-ext

--- app/src/main/java/com/example/moviedb/utils/Extensions.kt
+++ app/src/main/java/com/example/moviedb/utils/Extensions.kt
@@ -614,6 +614,67 @@ inline fun <reified T : Activity> Context.startActivityWithAnimation(enterResId:
     ContextCompat.startActivity(this, intent, bundle)
 }
 
+/**
+ * Kotlin Extensions for simpler, easier and funw way
+ * of launching of Activities
+ */
+
+inline fun <reified T : Any> Activity.launchActivity (
+        requestCode: Int = -1,
+        options: Bundle? = null,
+        noinline init: Intent.() -> Unit = {})
+{
+    val intent = newIntent<T>(this)
+    intent.init()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+    {
+        startActivityForResult(intent, requestCode, options)
+    } else {
+        startActivityForResult(intent, requestCode)
+    }
+}
+
+inline fun <reified T : Any> Context.launchActivity (
+        options: Bundle? = null,
+        noinline init: Intent.() -> Unit = {})
+{
+    val intent = newIntent<T>(this)
+    intent.init()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+    {
+        startActivity(intent, options)
+    } else {
+        startActivity(intent)
+    }
+}
+
+inline fun <reified T : Any> newIntent(context: Context): Intent =
+        Intent(context, T::class.java)
+
+// Simple Activities
+launchActivity<UserDetailActivity>()
+
+// Add Intent extras
+launchActivity<UserDetailActivity> {
+    putExtra(INTENT_USER_ID, user.id)
+}
+
+// Add custom flags
+launchActivity<UserDetailActivity> {
+    putExtra(INTENT_USER_ID, user.id)
+    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+}
+
+// Add Shared Transistions
+val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, avatar, "avatar")
+launchActivity<UserDetailActivity>(options = options) {
+    putExtra(INTENT_USER_ID, user.id)
+}
+
+// Add requestCode for startActivityForResult() call
+launchActivity<UserDetailActivity>(requestCode = 1234) {
+    putExtra(INTENT_USER_ID, user.id)
+}
 
 /**
  * Extension method to show toast for Context.
