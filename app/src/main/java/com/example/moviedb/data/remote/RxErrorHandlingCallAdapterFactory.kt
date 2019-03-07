a8384a5cf535935442a76db7aab94a2c4f002e22
Anh Quan <dang.anh.quan@framgia.com>
2019-03-07 13:57:32 +0700

    update-rx-error-factory

--- app/src/main/java/com/example/moviedb/data/remote/RxErrorHandlingCallAdapterFactory.kt
+++ app/src/main/java/com/example/moviedb/data/remote/RxErrorHandlingCallAdapterFactory.kt
@@ -50,46 +50,23 @@ class RxCallAdapterWrapper<R>(
         return when (result) {
             is Single<*> -> {
                 result.onErrorResumeNext(Function<Throwable, SingleSource<Nothing>> { throwable ->
-                    //                    Single.error<Nothing>(convertToRetrofitException(throwable))
                     Single.error<Nothing>(convertToBaseException(throwable))
                 })
             }
 
             is Observable<*> -> {
                 result.onErrorResumeNext(Function<Throwable, ObservableSource<Nothing>> { throwable ->
-                    //                    Observable.error<Nothing>(convertToRetrofitException(throwable))
                     Observable.error<Nothing>(convertToBaseException(throwable))
                 })
             }
 
             is Completable -> {
                 result.onErrorResumeNext { throwable ->
-                    //                    Completable.error(convertToRetrofitException(throwable))
                     Completable.error(convertToBaseException(throwable))
                 }
             }
 
             else -> result
-        }
-    }
-
-    private fun convertToRetrofitException(throwable: Throwable): RetrofitException {
-        return when (throwable) {
-            // We had non-200 http error
-            is HttpException -> {
-                val response = throwable.response()
-                RetrofitException.httpError(
-                    response.raw().request().url().toString(), response, retrofit
-                )
-            }
-            // A network error happened
-            is IOException -> {
-                RetrofitException.networkError(throwable)
-            }
-            // We don't know what happened. We need to simply convert to an unknown error
-            else -> {
-                RetrofitException.unexpectedError(throwable)
-            }
         }
     }
 
@@ -121,156 +98,77 @@ class RxCallAdapterWrapper<R>(
                 }
             }
 
-            else -> {
-                BaseException.toUnexpectedError(throwable)
-            }
+            else -> BaseException.toUnexpectedError(throwable)
         }
     }
 }
 
-class RetrofitException constructor(
-    message: String? = null,
-    /** The request URL which produced the error.  */
-    val url: String? = null,
-    /** Response object containing status code, headers, body, etc.  */
+class BaseException(
+    val errorType: ErrorType,
+    val errorResponse: BaseErrorResponse? = null,
     val response: Response<*>? = null,
-    /** The event kind which triggered this error.  */
-    val kind: Kind, exception: Throwable? = null,
-    /** The Retrofit this request was executed on  */
-    val retrofit: Retrofit? = null
-) : RuntimeException(message, exception) {
-
-    /** Identifies the event kind which triggered a [RetrofitException].  */
-    enum class Kind {
-        /** An [IOException] occurred while communicating to the server.  */
-        NETWORK,
-        /** A non-200 HTTP status code was received from the server.  */
-        HTTP,
-        /**
-         * An internal error occurred while attempting to execute a request. It is best practice to
-         * re-throw this exception so your application crashes.
-         */
-        UNEXPECTED
-    }
-
-    /**
-     * HTTP response body converted to specified `type`. `null` if there is no
-     * response.
-     *
-     * @throws IOException if unable to convert the body to the specified `type`.
-     */
-    @Throws(IOException::class)
-    fun <T> getErrorBodyAs(type: Class<T>): T? {
-        if (!(response?.errorBody() != null && retrofit != null)) {
-            return null
-        }
-        return retrofit.responseBodyConverter<T>(type, arrayOfNulls(0))
-            .convert(response.errorBody()!!)
-    }
-
-    companion object {
-        fun httpError(url: String, response: Response<*>, retrofit: Retrofit): RetrofitException {
-            val message = "${response.code()} ${response.message()}"
-            return RetrofitException(message, url, response, Kind.HTTP, null, retrofit)
-        }
-
-        fun networkError(exception: IOException): RetrofitException {
-            return RetrofitException(exception.message, null, null, Kind.NETWORK, exception, null)
-        }
-
-        fun unexpectedError(exception: Throwable): RetrofitException {
-            return RetrofitException(
-                exception.message,
-                null,
-                null,
-                Kind.UNEXPECTED,
-                exception,
-                null
-            )
-        }
-    }
-}
-
-class BaseException : RuntimeException {
-
-    private var errorType: Int = NETWORK
-    private var errorResponse: BaseErrorResponse? = null
-    var response: Response<*>? = null
+    cause: Throwable? = null
+) : RuntimeException(message = cause?.message, cause = cause) {
 
     val serverErrorCode: String
         get() = errorResponse?.code ?: ""
 
-    constructor(type: Int, cause: Throwable) : super(cause.message, cause) {
-        this.errorType = type
-    }
-
-    constructor(type: Int, errorResponse: BaseErrorResponse?) {
-        this.errorType = type
-        this.errorResponse = errorResponse
-    }
-
-    constructor(type: Int, response: Response<*>?) {
-        this.errorType = type
-        this.response = response
-    }
-
     override val message: String?
         get() = when (errorType) {
-            SERVER -> errorResponse?.errors?.getOrNull(0)?.name?.getOrNull(0)
+            ErrorType.SERVER -> errorResponse?.errors?.getOrNull(0)?.name?.getOrNull(0)
 
-            NETWORK -> cause?.message
+            ErrorType.NETWORK -> cause?.message
 
-            HTTP -> response?.message()
+            ErrorType.HTTP -> response?.message()
 
-            UNEXPECTED -> cause?.message
-
-            else -> {
-                "Unexpected error has occurred"
-            }
+            ErrorType.UNEXPECTED -> cause?.message
         }
 
     companion object {
+        fun toHttpError(response: Response<*>): BaseException =
+            BaseException(errorType = ErrorType.HTTP, response = response)
 
-        /**
-         * An [IOException] occurred while communicating to the server.
-         */
-        const val NETWORK = 0
-        /**
-         * A non-2xx HTTP status code was received from the server.
-         */
-        const val HTTP = 1
-        /**
-         * A error server with code & message
-         */
-        const val SERVER = 2
-        /**
-         * An internal error occurred while attempting to execute a request. It is best practice to
-         * re-throw this exception so your application crashes.
-         */
-        const val UNEXPECTED = 3
+        fun toNetworkError(cause: Throwable): BaseException =
+            BaseException(errorType = ErrorType.NETWORK, cause = cause)
 
-        fun toHttpError(response: Response<*>): BaseException {
-            return BaseException(HTTP, response)
-        }
+        fun toServerError(errorResponse: BaseErrorResponse): BaseException =
+            BaseException(errorType = ErrorType.SERVER, errorResponse = errorResponse)
 
-        fun toNetworkError(cause: Throwable): BaseException {
-            return BaseException(NETWORK, cause)
-        }
-
-        fun toServerError(errorResponse: BaseErrorResponse): BaseException {
-            return BaseException(SERVER, errorResponse)
-        }
-
-        fun toUnexpectedError(cause: Throwable): BaseException {
-            return BaseException(UNEXPECTED, cause)
-        }
+        fun toUnexpectedError(cause: Throwable): BaseException =
+            BaseException(errorType = ErrorType.UNEXPECTED, cause = cause)
     }
+}
+
+/**
+ * Identifies the error type which triggered a [BaseException]
+ */
+enum class ErrorType {
+    /**
+     * An [IOException] occurred while communicating to the server.
+     */
+    NETWORK,
+
+    /**
+     * A non-2xx HTTP status code was received from the server.
+     */
+    HTTP,
+
+    /**
+     * A error server with code & message
+     */
+    SERVER,
+
+    /**
+     * An internal error occurred while attempting to execute a request. It is best practice to
+     * re-throw this exception so your application crashes.
+     */
+    UNEXPECTED
 }
 
 class BaseErrorResponse(
     @SerializedName("code") var code: String? = null,
-    @SerializedName("message") var message: String? = null,
-    @SerializedName("errors") var errors: List<Error>? = null
+    @SerializedName("message") val message: String? = null,
+    @SerializedName("errors") val errors: List<Error>? = null
 )
 
 class Error(
