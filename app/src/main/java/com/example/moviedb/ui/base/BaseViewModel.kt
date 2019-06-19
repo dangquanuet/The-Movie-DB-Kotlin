fccdb4cdcba1eca19d379f7e711efe5c6543bff9
Anh Quan <dangquanuet@gmail.com>
2019-06-19 16:04:38 +0700

    minor-update

--- app/src/main/java/com/example/moviedb/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/example/moviedb/ui/base/BaseViewModel.kt
@@ -2,10 +2,10 @@ package com.example.moviedb.ui.base
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.moviedb.data.remote.BaseException
 import com.example.moviedb.utils.SingleLiveEvent
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
@@ -20,25 +20,27 @@ abstract class BaseViewModel : ViewModel() {
     val forceUpdateAppEvent = SingleLiveEvent<Unit>()
     val serverMaintainEvent = SingleLiveEvent<Unit>()
 
+
     // rx
 //    private val compositeDisposable = CompositeDisposable()
 //    fun addDisposable(disposable: Disposable) = compositeDisposable.add(disposable)
 
     // coroutines
-    /*private val viewModelJob = SupervisorJob()
-    protected val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+    protected val exceptionHandler = CoroutineExceptionHandler { context, throwable ->
         viewModelScope.launch {
             onLoadFail(throwable)
         }
     }
+    /*
+    private val viewModelJob = SupervisorJob()
     protected val ioContext = viewModelJob + Dispatchers.IO
     protected val uiContext = viewModelJob + Dispatchers.Main
     protected val ioScope = CoroutineScope(ioContext)
     protected val uiScope = CoroutineScope(uiContext)
     protected val ioScopeError = CoroutineScope(ioContext + exceptionHandler)
     protected val uiScopeError = CoroutineScope(uiContext + exceptionHandler)
-
-    protected val viewModelScopeExceptionHandler = viewModelScope + exceptionHandler*/
+    */
+    protected val viewModelScopeExceptionHandler = viewModelScope + exceptionHandler
 
     open suspend fun onLoadFail(throwable: Throwable) {
         withContext(Dispatchers.Main) {
--- app/src/main/java/com/example/moviedb/ui/screen/tv/TvListViewModel.kt
+++ app/src/main/java/com/example/moviedb/ui/screen/tv/TvListViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.moviedb.ui.screen.tv
 
-import androidx.lifecycle.viewModelScope
 import com.example.moviedb.data.model.Tv
 import com.example.moviedb.data.remote.ApiParams
 import com.example.moviedb.data.repository.UserRepository
@@ -19,45 +18,9 @@ class TvListViewModel(
         getTv4(page, hashMap)
     }
 
-    /*fun getTv(page: Int, hashMap: HashMap<String, String>) {
-        userRepository.getTvList(hashMap, {
-            onLoadSuccess(page, it.results)
-        }, {
-            onLoadFail(it)
-        })
-    }
-
-    fun getTv2(page: Int, hashMap: HashMap<String, String>) {
-        ioScope.launch {
-            val result = userRepository.getTvList2(hashMap)
-            when (result) {
-                is Result.Success<GetTvListResponse> -> {
-                    onLoadSuccess(page, result.data.results)
-                }
-                is Result.Error -> {
-                    onLoadFail(result.error)
-                }
-            }
-        }
-    }
-
-    fun getTv3(page: Int, hashMap: HashMap<String, String>) {
-        uiScope.launch {
-            try {
-                onLoadSuccess(page, userRepository.getTvList3((hashMap)).results)
-            } catch (e: Exception) {
-                onLoadFail(e)
-            }
-        }
-    }*/
-
     fun getTv4(page: Int, hashMap: HashMap<String, String>) {
-        viewModelScope.launch {
-            try {
-                onLoadSuccess(page, userRepository.getTvList3((hashMap)).results)
-            } catch (throwable: Throwable) {
-                onLoadFail(throwable)
-            }
+        viewModelScopeExceptionHandler.launch {
+            onLoadSuccess(page, userRepository.getTvList3((hashMap)).results)
         }
     }
 
