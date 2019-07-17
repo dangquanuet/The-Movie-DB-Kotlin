60ce49e00ef6c6d62b1860e40a8bf69127e5fbbc
Anh Quan <dangquanuet@gmail.com>
2019-07-17 13:35:39 +0700

    minor-update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,6 @@ dependencies {
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     implementation("androidx.recyclerview:recyclerview:1.0.0")
-    implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.0.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinCompilerVersion.VERSION}")
     implementation("androidx.core:core-ktx:1.0.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
         classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0")
     }
 }
