330ee9c25375373c57e9fd50f472911db8228a6b
Anh Quan <dangquanuet@gmail.com>
2019-08-27 11:05:17 +0700

    minor-update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.1.0-rc01")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.recyclerview:recyclerview:1.1.0-beta02")
+    implementation("androidx.recyclerview:recyclerview:1.1.0-beta03")
     implementation("com.google.android.material:material:1.1.0-alpha09")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:${KotlinCompilerVersion.VERSION}")
     implementation("androidx.core:core-ktx:1.2.0-alpha03")
