b69d04e83b762dc6027320082f304c69a0500cd9
Anh Quan <dangquanuet@gmail.com>
2019-09-05 14:42:17 +0700

    update-kgp-1-3-50

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.0-alpha01")
     }
 }
