aa0e0e888a51a186914b2e621f05190b04fb94ab
Anh Quan <dangquanuet@gmail.com>
2019-10-03 15:40:33 +0700

    minor-update

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath("com.android.tools.build:gradle:3.5.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.0-alpha03")
     }
