43dceba03555d25381ced1aae9c3a2eb2cb04247
Anh Quan <dangquanuet@gmail.com>
2020-04-01 09:03:00 +0700

    Update koin module

--- app/src/main/java/com/example/moviedb/data/local/pref/AppPrefs.kt
+++ app/src/main/java/com/example/moviedb/data/local/pref/AppPrefs.kt
@@ -1,22 +1,17 @@
 package com.example.moviedb.data.local.pref
 
-import android.content.Context
+import android.content.SharedPreferences
 import androidx.core.content.edit
 import com.google.gson.Gson
 
 class AppPrefs(
-    context: Context,
+    private val sharedPreferences: SharedPreferences,
     val gson: Gson
 ) : PrefHelper {
 
     companion object {
         private const val FIRST_RUN = "FIRST_RUN"
     }
-
-    private val sharedPreferences = context.getSharedPreferences(
-        context.packageName,
-        Context.MODE_PRIVATE
-    )
 
     override fun isFirstRun(): Boolean {
         val isFirstRun = sharedPreferences.getBoolean(FIRST_RUN, true)
--- app/src/main/java/com/example/moviedb/di/RepositoryModule.kt
+++ app/src/main/java/com/example/moviedb/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package com.example.moviedb.di
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.example.moviedb.data.constants.Constants
 import com.example.moviedb.data.local.db.AppDatabase
@@ -12,17 +13,23 @@ import com.google.gson.Gson
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { createDatabaseName() }
-    single { createAppDatabase(get(), get()) }
+    // database
+    single { createAppDatabase(get()) }
     single { createMovieDao(get()) }
+
+    // shared prefs
+    single { createSharedPrefs(get()) }
     single<PrefHelper> { AppPrefs(get(), get()) }
     single { Gson() }
+
+    // repository
     single<UserRepository> { UserRepositoryImpl(get(), get()) }
 }
 
-fun createDatabaseName() = Constants.DATABASE_NAME
+fun createSharedPrefs(context: Context): SharedPreferences =
+    context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
 
-fun createAppDatabase(dbName: String, context: Context) =
-    Room.databaseBuilder(context, AppDatabase::class.java, dbName).build()
+fun createAppDatabase(context: Context) =
+    Room.databaseBuilder(context, AppDatabase::class.java, Constants.DATABASE_NAME).build()
 
 fun createMovieDao(appDatabase: AppDatabase) = appDatabase.movieDao()
